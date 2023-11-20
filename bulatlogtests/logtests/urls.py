c74e117e4bb90ccc204664bd929b2570ad5b215c
SergeyVolkoff <vol7745@yandex.ru>
2023-11-20 10:01:37 -0500

    bu

--- bulatlogtests/logtests/urls.py
+++ bulatlogtests/logtests/urls.py
@@ -1,8 +1,9 @@
 from . import views
-from django.urls import path
+from django.urls import path,re_path,register_converter
 
 urlpatterns = [
     path('', views.index),
     path('statistic/<int:stat_id>/', views.statistic_testing),
     path('statistic/<slug:stat_slug>/', views.statistic_by_slug),
+    path('statistic_data/<int:test_data_id>/', views.show_test_data, name='data_tests'),
 ]
--- bulatlogtests/logtests/views.py
+++ bulatlogtests/logtests/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.template.defaultfilters import slugify
+
 
 menu = ['About tests', 'Result']
 
+data_db = [
+    {'id':1,'title':'test OSPF', 'content':'result test OSPF','is_published': True},
+    {'id':2,'title':'test rip', 'content':'result test RIP','is_published':True},
+    {'id':3,'title':'test bgp', 'content':'result test BGP','is_published':True},
+    ]
 
 def index(request):
     # return HttpResponse("<h1>TESTS SUITE LOG</h1>")
     data= {
         'title': 'TESTS SUITE LOG',
         'menu': menu,
+        'suite_test':data_db
     }
     return render(request,'logtests/index.html',context = data)
 
@@ -17,3 +26,9 @@ def statistic_testing(request,stat_id):
 
 def statistic_by_slug(request, stat_slug):
     return HttpResponse(f"Test Statistics<p>slug: {stat_slug}</p>")
+
+def show_test_data(request,test_data_id):
+    return HttpResponse(f"View data test id={test_data_id}")
+
+def page_not_found(request, exception):
+    return HttpResponse("<h1>No test page</h1>")
