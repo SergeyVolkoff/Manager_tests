0f3c8b26389a1db1b7d46fd11426737619b71d23
SergeyVolkoff <vol7745@yandex.ru>
2023-11-30 14:39:49 -0500

    bu

--- bulatlogtests/logtests/models.py
+++ bulatlogtests/logtests/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Test_Statistics(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    time_create = models.DateField(auto_now_add=True)
+    time_update = models.DateField(auto_now=True)
+    is_published = models.BooleanField(default=True)
